fdf695e8282c6fb11962b5b105d15890a34292f7
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-02-13 15:19:01 -0300

    use ref

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import { useState,useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
-const UseEffect = (props) => {
+function calcFatorial(n){
+    if (n < 0) return -1
+    if (n === 0) return 1
+    return calcFatorial(n-1) * n
+}
+
+const UseEffect = () => {
+    const [number, setNumber] = useState(1);
+    const [fatorial, setFatorial] = useState(1);
+    const [isPar, setPar] = useState(false);
+    useEffect(function(){
+        setFatorial(calcFatorial(number))
+        setPar(number % 2 === 0 ? true : false);
+    }, [number]);
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercício 01" />
+            <div className="bg">
+                <p>{`useEffect(function(){`}</p>
+                <p>{`setFatorial(calcFatorial(number))`}</p>
+                <p>{`}, [number]);`}</p>
+                <p>{`Função será executada toda vez que a const number for alterada`}</p>
+            </div>
+            <div className="center">
+                <div><span className="text">Fatorial:</span>
+                    <span className="text red">{fatorial}</span></div>
+                <input type="number" className="input" value={number} onChange={e => setNumber(e.target.value)} />
+            </div>
+            <SectionTitle title="Exercício 02 (Desafio)" />
+            <div className="bg">
+                <p>Fazer checage se o número do input é par ou impar</p>
+            </div>
+            <div className="center">
+                <span className="text">{number} é {isPar ? "par" : "ímpar"}</span>
+            </div>
         </div>
     )
 }
--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,74 @@
-import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { useEffect, useRef, useState } from 'react';
 
-const UseRef = (props) => {
+const UseRef = () => {
+    const [value1, SetValue1] = useState("");
+    const [value2, SetValue2] = useState("");
+    const [value3, SetValue3] = useState("");
+    const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
+
+    useEffect(function () {
+        count.current = count.current + 1
+        if (myInput2.current) { // Check if myInput2.current is not null
+            myInput2.current.focus();
+        }
+        SetValue3(merge(value1, value2))
+    }, [value1])
+
+    useEffect(function () {
+        count.current++
+        if (myInput1.current) { // Check if myInput1.current is not null
+            myInput1.current.focus();
+        }
+        SetValue3(merge(value1, value2))
+    }, [value2])
+
+    function merge(s1, s2) {
+        let resultado = '';
+        for (let index = 0; index < Math.max(s1.length, s2.length); index++) {
+            if (index < s1.length) {
+                resultado += s1[index]
+            }
+            if (index < s2.length) {
+                resultado += s2[index]
+            }
+        }
+        return resultado
+    }
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="Exercicio 1" />
+            <div className="bg">
+                <p>{`const count = useRef(0) `}</p>
+                <p>{`count.current = count.current + 1 // adiciona 1 ao current cada vez que o a propriedade é renderizada`}</p>
+            </div>
+
+            <div className="center">
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text">{value3} [</span>
+                    <span className="text red">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+
+                <input type="text" ref={myInput1} className="input" value={value1} onChange={e => SetValue1(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercicio 2" />
+            <div className="bg">
+                <p>1. Juntar as duas strings alternando as letras de cada</p>
+            </div>
+            <div className="center">
+                <input type="text" ref={myInput2} className="input" value={value2} onChange={e => SetValue2(e.target.value)} />
+            </div>
         </div>
     )
 }
